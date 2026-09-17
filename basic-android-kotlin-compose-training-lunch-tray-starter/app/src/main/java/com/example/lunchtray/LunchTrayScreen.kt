a824459d45d1ecda21181ad0fbf84e7b816a5c1f
gabrielcuadros-cmyk <gabriel.cuadros@tecsup.edu.pe>
2026-09-17 13:15:44 -0500

    Paso 3: Definicion del enum LunchTrayScreen con las 5 rutas y sus titulos

--- basic-android-kotlin-compose-training-lunch-tray-starter/app/src/main/java/com/example/lunchtray/LunchTrayScreen.kt
+++ basic-android-kotlin-compose-training-lunch-tray-starter/app/src/main/java/com/example/lunchtray/LunchTrayScreen.kt
@@ -15,6 +15,7 @@
  */
 package com.example.lunchtray
 
+import androidx.annotation.StringRes
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
@@ -23,7 +24,14 @@ import androidx.compose.runtime.getValue
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.lunchtray.ui.OrderViewModel
 
-// TODO: Screen enum
+// Enum que define las rutas de la app y asocia cada pantalla con su título de los recursos de cadenas
+enum class LunchTrayScreen(@StringRes val title: Int) {
+    Start(title = R.string.app_name),
+    Entree(title = R.string.choose_entree),
+    SideDish(title = R.string.choose_side_dish),
+    Accompaniment(title = R.string.choose_accompaniment),
+    Checkout(title = R.string.order_checkout)
+}
 
 // TODO: AppBar
 
